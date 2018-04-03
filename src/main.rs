9c32dae1f952486f0befed7676766951a922d5f4
Andrew R Martin <0andrewmartin0@gmail.com>
2018-04-03 01:12:07 +0100

    got code sample to work

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,41 @@
+extern crate sdl2;
 
-fn main() {
-    println!("Hello, world!");
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
+
+pub fn main() {
+
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let window = video_subsystem.window("rust-sdl2 demo: Cursor", 800, 600)
+      .position_centered()
+      .build()
+      .unwrap();
+
+    let mut canvas = window.into_canvas().software().build().unwrap();
+
+    canvas.clear();
+    canvas.present();
+
+    canvas.set_draw_color(Color::RGBA(255, 255, 255, 255));
+
+    let mut events = sdl_context.event_pump().unwrap();
+
+    'mainloop: loop {
+        for event in events.poll_iter() {
+            match event {
+                Event::Quit{..} |
+                Event::KeyDown {keycode: Option::Some(Keycode::Escape), ..} =>
+                    break 'mainloop,
+                 Event::MouseButtonDown {x, y, ..} => {
+                    canvas.fill_rect(Rect::new(x, y, 5, 5)).unwrap();
+                    canvas.present();
+                }
+                _ => {}
+            }
+        }
+    }
 }
+
