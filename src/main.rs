be2fb1ba71ba05f8f84d28992819def9db1c8bd5
Andrew Martin <0andrewmartin0@gmail.com>
2018-04-04 19:11:23 +0100

    do font stuff

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate sdl2;
+extern crate rusttype;
 
 use sdl2::event::Event;
 use sdl2::event::WindowEvent;
@@ -13,7 +14,95 @@ type Canvas = sdl2::render::Canvas<sdl2::video::Window>;
 static BOX_W : i32 = 400;
 static BOX_H : i32 = 300;
 
-fn draw_stuff(w : i32, h : i32, text : &mut string, canvas : &mut Canvas){
+use rusttype::{point, FontCollection, PositionedGlyph, Scale};
+use std::io::Write;
+
+fn draw_font() -> (Vec<u8>, i32, i32) {
+  let font_data = include_bytes!("../fonts/consola.ttf");
+  let collection = FontCollection::from_bytes(font_data as &[u8]).unwrap_or_else(|e| {
+    panic!("error constructing a FontCollection from bytes: {}", e);
+  });
+  let font = collection.into_font() // only succeeds if collection consists of one font
+    .unwrap_or_else(|e| {
+      panic!("error turning FontCollection into a Font: {}", e);
+    });
+
+  // Desired font pixel height
+  let height: f32 = 12.4; // to get 80 chars across (fits most terminals); adjust as desired
+  let pixel_height = height.ceil() as usize;
+
+  // 2x scale in x direction to counter the aspect ratio of monospace characters.
+  let scale = Scale {
+    x: height * 2.0,
+    y: height,
+  };
+
+  // The origin of a line of text is at the baseline (roughly where
+  // non-descending letters sit). We don't want to clip the text, so we shift
+  // it down with an offset when laying it out. v_metrics.ascent is the
+  // distance between the baseline and the highest edge of any glyph in
+  // the font. That's enough to guarantee that there's no clipping.
+  let v_metrics = font.v_metrics(scale);
+  let offset = point(0.0, v_metrics.ascent);
+
+  // Glyphs to draw for "RustType". Feel free to try other strings.
+  let glyphs: Vec<PositionedGlyph> = font.layout("RustType", scale, offset).collect();
+
+  // Find the most visually pleasing width to display
+  let width = glyphs
+    .iter()
+    .rev()
+    .map(|g| g.position().x as f32 + g.unpositioned().h_metrics().advance_width)
+    .next()
+    .unwrap_or(0.0)
+    .ceil() as usize;
+
+  // Rasterise directly into ASCII art.
+  let mut pixel_data = vec![b'@'; width * pixel_height];
+  let mapping = b"@%#x+=:-. "; // The approximation of greyscale
+  let mapping_scale = (mapping.len() - 1) as f32;
+  for g in glyphs {
+    if let Some(bb) = g.pixel_bounding_box() {
+      g.draw(|x, y, v| {
+        // v should be in the range 0.0 to 1.0
+        let i = (v * mapping_scale + 0.5) as usize;
+        // so something's wrong if you get $ in the output.
+        let c = mapping.get(i).cloned().unwrap_or(b'$');
+        let x = x as i32 + bb.min.x;
+        let y = y as i32 + bb.min.y;
+        // There's still a possibility that the glyph clips the boundaries of the bitmap
+        if x >= 0 && x < width as i32 && y >= 0 && y < pixel_height as i32 {
+          let x = x as usize;
+          let y = y as usize;
+          pixel_data[(x + y * width)] = c;
+        }
+      })
+    }
+  }
+
+  return (pixel_data, width as i32, pixel_height as i32);
+
+  /* Print it out
+  let stdout = ::std::io::stdout();
+  let mut handle = stdout.lock();
+  for j in 0..pixel_height {
+    handle
+      .write(&pixel_data[j * width..(j + 1) * width])
+      .unwrap();
+    handle.write(b"\n").unwrap();
+  }
+  */
+}
+
+fn draw_text(){
+  let (pixels, width, height) = draw_font();
+  //let surface = Surface::new(512, 512, PixelFormatEnum::RGB24).unwrap();
+  println!("width: {}, height: {}", width, height);
+}
+
+
+
+fn draw_stuff(w : i32, h : i32, text : &mut String, canvas : &mut Canvas){
 	let (rw, rh) = (BOX_W, BOX_H);
 	let x = (w/2) - (rw/2);
 	let y = (h/2) - (rh/2);
@@ -26,7 +115,7 @@ pub fn main() {
 
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
-    let window = video_subsystem.window("rust-sdl2 demo: Cursor", width, height)
+    let window = video_subsystem.window("cauldron", width, height)
       .position_centered()
       .resizable()
       .build()
@@ -36,6 +125,8 @@ pub fn main() {
 
     canvas.clear();
     canvas.present();
+
+    draw_text();
 
     let mut events = sdl_context.event_pump().unwrap();
 
@@ -47,45 +138,45 @@ pub fn main() {
     let mut text = "Hello World".to_string();
 
     'mainloop: loop {
-        for event in events.poll_iter() {
-            match event {
-                Event::Quit{..} |
-                Event::KeyDown {keycode: Option::Some(Keycode::Escape), ..} =>
-                    break 'mainloop,
-	            Event::MouseButtonUp {x, y, ..} => {
-	             	let xp = cmp::min(x, xd);
-	             	let yp = cmp::min(y, yd);
-	             	let w = (x - xd).abs() as u32;
-	             	let h = (y - yd).abs() as u32;
-	             	rects.push(Rect::new(xp, yp, w, h));
+      for event in events.poll_iter() {
+        match event {
+          Event::Quit{..} |
+          Event::KeyDown {keycode: Option::Some(Keycode::Escape), ..} =>
+              break 'mainloop,
+	        Event::MouseButtonUp {x, y, ..} => {
+	          let xp = cmp::min(x, xd);
+	          let yp = cmp::min(y, yd);
+	          let w = (x - xd).abs() as u32;
+	          let h = (y - yd).abs() as u32;
+	          rects.push(Rect::new(xp, yp, w, h));
+	        },
+	        Event::MouseButtonDown {x, y, ..} => {
+	            xd = x;
+	            yd = y;
+	        },
+	        Event::Window { win_event, .. } => {
+	          match win_event {
+	            WindowEvent::Resized(x, y) => {
+	            	width = x as u32;
+	            	height = y as u32;
 	            },
-	            Event::MouseButtonDown {x, y, ..} => {
-	                xd = x;
-	                yd = y;
-	            }
-	            Event::Window { win_event, .. } => {
-	            	match win_event {
-	            		WindowEvent::Resized(x, y) => {
-	            			width = x as u32;
-	            			height = y as u32;
-	            		},
-	            		_ => {}
-	            	}
-	            }
-                _e => {}
-            }
+	            _ => {}
+	          }
+	        },
+          _e => {}
         }
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
-        // The rest of the loop goes here...
+      }
+      ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+      // The rest of the loop goes here...
 
-		canvas.set_draw_color(Color::RGBA(0, 0, 0, 255));
-        canvas.clear();
-        canvas.set_draw_color(Color::RGBA(255, 255, 255, 255));
-        for r in rects.iter() {
-        	canvas.fill_rect(*r).unwrap();
+		  canvas.set_draw_color(Color::RGBA(0, 0, 0, 255));
+      canvas.clear();
+      canvas.set_draw_color(Color::RGBA(255, 255, 255, 255));
+      for r in rects.iter() {
+        canvas.fill_rect(*r).unwrap();
     	}
-        draw_stuff(width as i32, height as i32, &mut text, &mut canvas);
-        canvas.present();
+      draw_stuff(width as i32, height as i32, &mut text, &mut canvas);
+      canvas.present();
 	}
 }
 
