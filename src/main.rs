322801fb3d22c5955d64e6bdf5e150ee3c31e294
nepp <0andrewmartin0@gmail.com>
2018-09-03 16:56:44 +0100

    move unused source files to legacy folder

--- src/main.rs
+++ src/main.rs
@@ -12,19 +12,13 @@ extern crate notify;
 #[macro_use]
 extern crate lazy_static;
 
-mod font_render;
-mod text_edit;
 mod lexer;
 mod parser;
 mod interpreter;
-mod visual_edit;
 mod repl;
 mod watcher;
 
 fn main(){
   watcher::watch("code.txt");
   //repl::repl();
-  //visual_edit::run_sdl2_app();
-  //parser::test_parse();
-  //interpreter::test_interpret();
 }
