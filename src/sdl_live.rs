96a8c209679eb316f20f5817cfe3ac464536988b
Andrew Martin <0andrewmartin0@gmail.com>
2018-10-19 18:33:50 +0100

    sdl_live stuff

--- src/sdl_live.rs
+++ src/sdl_live.rs
@@ -1,5 +1,6 @@
 
 use sdl2;
+use sdl2::{Sdl, VideoSubsystem, EventPump};
 use sdl2::event::Event;
 use sdl2::event::WindowEvent;
 use sdl2::keyboard::{Keycode, Scancode, KeyboardState};
@@ -14,38 +15,29 @@ fn dpi_ratio(w : &Window) -> f32 {
   (w as f32) / (dw as f32)
 }
 
-pub fn run_sdl2_app() {
+pub type Canvas = sdl2::render::Canvas<sdl2::video::Window>;
 
-	let (width, height) = (1800, 900);
+pub struct SdlView {
+  pub sdl : Sdl,
+  pub video : VideoSubsystem, 
+  pub dpi_ratio : f32,
+  pub canvas : Canvas,
+  pub events : EventPump,
+}
 
-  let sdl_context = sdl2::init().unwrap();
-  let video_subsystem = sdl_context.video().unwrap();
+pub fn init (width : u32, height : u32) -> SdlView {
+  let sdl = sdl2::init().unwrap();
+  let video = sdl.video().unwrap();
 
-  let window = video_subsystem.window("demo", width, height)
+  let window = video.window("demo", width, height)
     .position_centered()
     .resizable()
     .build()
     .unwrap();
 
   let dpi_ratio = dpi_ratio(&window);
+  let canvas = window.into_canvas().accelerated().build().unwrap();
+  let events = sdl.event_pump().unwrap();
 
-  let mut canvas = window.into_canvas().accelerated().build().unwrap();
-
-  canvas.set_blend_mode(BlendMode::Blend);
-
-  canvas.clear();
-  canvas.present();
-
-  let mut events = sdl_context.event_pump().unwrap();
-
-  let mut texture_creator = canvas.texture_creator();
-  
-  'mainloop: loop {
-
-    for event in events.poll_iter() {
-      // handle event
-    }
-
-    ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
-	}
+  SdlView { sdl, video, dpi_ratio, canvas, events }
 }
