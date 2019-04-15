1ec755767e387a1b64a7f62d06a680a0ab48c3a9
nepp <0andrewmartin0@gmail.com>
2019-04-15 23:48:15 +0100

    more ffi stuff

--- c_bind_test/target/debug/c_bind_test.d
+++ c_bind_test/target/debug/c_bind_test.d
@@ -1,1 +1,1 @@
-C:\Users\andrew\workspace\cauldron\c_bind_test\target\debug\c_bind_test.pdb: C:\Users\andrew\workspace\cauldron\c_bind_test\src\main.rs
+C:\Users\Andy\Workspace\cauldron\c_bind_test\target\debug\c_bind_test.pdb: C:\Users\Andy\Workspace\cauldron\c_bind_test\src\main.rs
