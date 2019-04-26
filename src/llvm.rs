ff67883abb1ea7269a708e9d295f2eb4ff84ba8f
Andrew Martin <0andrewmartin0@gmail.com>
2019-04-26 17:44:42 +0100

    start generating LLVM IR

--- src/llvm.rs
+++ src/llvm.rs
@@ -33,22 +33,105 @@ from Jauhien Piatlicki.]
 
 */
 
-/*
-TODO:
-  * Change to the Rust repl thingy I tried to use before
-
-*/
-
 use std::io;
 use std::io::Write;
 
 use rustyline::error::ReadlineError;
 use rustyline::Editor;
 
-use crate::value::{SymbolTable, display_expr};
+use crate::error::{Error, error};
+use crate::value::{SymbolTable, display_expr, RefStr, Expr, ExprTag};
 use crate::lexer;
 use crate::parser;
 use crate::parser::ReplParseResult::{Complete, Incomplete};
+
+use std::collections::HashMap;
+
+use inkwell::basic_block::BasicBlock;
+use inkwell::builder::Builder;
+use inkwell::context::{Context, ContextRef};
+use inkwell::module::Module;
+use inkwell::passes::PassManager;
+use inkwell::types::BasicTypeEnum;
+use inkwell::values::{BasicValueEnum, FloatValue, FunctionValue, PointerValue, GenericValue};
+use inkwell::{OptimizationLevel, FloatPredicate};
+
+pub struct Ctx {
+  context: ContextRef,
+  builder: Builder,
+  named_values: HashMap<RefStr, PointerValue>,
+}
+
+impl Ctx {
+  pub fn new(sym : &mut SymbolTable) -> Ctx {
+    Ctx {
+      context: Context::get_global(),
+      builder: Builder::create(),
+      named_values: HashMap::new(),
+    }
+  }
+}
+
+pub trait ModuleProvider {
+    fn dump(&self);
+    fn get_module(&mut self) -> &mut Module;
+    fn get_function(&mut self, name: &str) -> Option<(FunctionValue, bool)>;
+}
+
+pub struct SimpleModuleProvider {
+  module : Module,
+}
+
+impl SimpleModuleProvider {
+    pub fn new(name: &str) -> SimpleModuleProvider {
+        SimpleModuleProvider {
+            module: Module::create(name)
+        }
+    }
+}
+
+impl ModuleProvider for SimpleModuleProvider {
+    fn dump(&self) {
+      println!("{}", self.module.print_to_string())
+    }
+
+    fn get_module(&mut self) -> &mut Module {
+        &mut self.module
+    }
+
+    fn get_function(&mut self, name: &str) -> Option<(FunctionValue, bool)> {
+      self.module.get_function(name)
+        .map(|f| (f, f.count_basic_blocks() > 0))
+    }
+}
+
+fn codegen_expression(expr : &Expr, ctx: &mut Ctx, module_provider: &mut ModuleProvider, sym : &mut SymbolTable) -> Result<FloatValue, Error> {
+  match &expr.tag {
+    ExprTag::Tree(_) => {
+      let instr = sym.str(expr.tree_symbol_unwrap()?);
+      let children = expr.children.as_slice();
+      match (instr, children) {
+        ("call", [op, a, b]) => {
+          let lhs_value = codegen_expression(a, ctx, module_provider, sym)?;
+          let rhs_value = codegen_expression(b, ctx, module_provider, sym)?;
+          let op_str = sym.str(op.symbol_unwrap()?);
+          match op_str {
+            "+" => Ok(ctx.builder.build_float_add(lhs_value, rhs_value, "add_result")),
+            "-" => Ok(ctx.builder.build_float_sub(lhs_value, rhs_value, "sub_result")),
+            "*" => Ok(ctx.builder.build_float_mul(lhs_value, rhs_value, "mul_result")),
+            "/" => Ok(ctx.builder.build_float_div(lhs_value, rhs_value, "div_result")),
+            _ => panic!(),
+          }
+        }
+        _ => panic!(),
+      }
+    }
+    ExprTag::LiteralFloat(f) => {
+      Ok(ctx.context.f64_type().const_float(*f as f64))
+    }
+    _ => panic!(),
+  }
+}
 
 pub fn run_repl() {
   let mut sym = SymbolTable::new();
