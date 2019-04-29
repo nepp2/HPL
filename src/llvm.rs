f85275060a5886c642b7be5429be116cfe5e7f1c
Andrew Martin <0andrewmartin0@gmail.com>
2019-04-29 21:15:15 +0100

    code for generating functions (half done)

--- src/llvm.rs
+++ src/llvm.rs
@@ -46,20 +46,21 @@ use crate::parser;
 use crate::parser::ReplParseResult::{Complete, Incomplete};
 
 use std::collections::HashMap;
+use itertools::Itertools;
 
 use inkwell::basic_block::BasicBlock;
 use inkwell::builder::Builder;
 use inkwell::context::{Context, ContextRef};
 use inkwell::module::Module;
 use inkwell::passes::PassManager;
-use inkwell::types::BasicTypeEnum;
-use inkwell::values::{BasicValueEnum, FloatValue, FunctionValue, PointerValue, GenericValue};
+use inkwell::types::{BasicTypeEnum, AnyTypeEnum};
+use inkwell::values::{BasicValueEnum, AnyValueEnum, FloatValue, FunctionValue, PointerValue, GenericValue};
 use inkwell::{OptimizationLevel, FloatPredicate};
 
 pub struct Ctx {
   context: ContextRef,
   builder: Builder,
-  named_values: HashMap<RefStr, PointerValue>,
+  named_values: HashMap<RefStr, BasicValueEnum>,
 }
 
 impl Ctx {
@@ -72,64 +73,122 @@ impl Ctx {
   }
 }
 
-pub trait ModuleProvider {
-    fn dump(&self);
-    fn get_module(&mut self) -> &mut Module;
-    fn get_function(&mut self, name: &str) -> Option<(FunctionValue, bool)>;
+fn dump_module(module : &Module) {
+  println!("{}", module.print_to_string())
 }
 
-pub struct SimpleModuleProvider {
-  module : Module,
+fn codegen_float(e : &Expr, ctx: &mut Ctx, module: &mut Module, sym : &mut SymbolTable) -> Result<FloatValue, Error> {
+  let v = codegen_expression(e, ctx, module, sym)?;
+  match v {
+    BasicValueEnum::FloatValue(f) => Ok(f),
+    t => error(e, format!("Expected float, found {:?}", t)),
+  }
 }
 
-impl SimpleModuleProvider {
-    pub fn new(name: &str) -> SimpleModuleProvider {
-        SimpleModuleProvider {
-            module: Module::create(name)
-        }
-    }
-}
-
-impl ModuleProvider for SimpleModuleProvider {
-    fn dump(&self) {
-      println!("{}", self.module.print_to_string())
-    }
-
-    fn get_module(&mut self) -> &mut Module {
-        &mut self.module
-    }
-
-    fn get_function(&mut self, name: &str) -> Option<(FunctionValue, bool)> {
-      self.module.get_function(name)
-        .map(|f| (f, f.count_basic_blocks() > 0))
-    }
-}
-
-fn codegen_expression(expr : &Expr, ctx: &mut Ctx, module_provider: &mut ModuleProvider, sym : &mut SymbolTable) -> Result<FloatValue, Error> {
+fn codegen_expression(expr : &Expr, ctx: &mut Ctx, module: &mut Module, sym : &mut SymbolTable) -> Result<BasicValueEnum, Error> {
   match &expr.tag {
     ExprTag::Tree(_) => {
       let instr = sym.str(expr.tree_symbol_unwrap()?);
       let children = expr.children.as_slice();
       match (instr, children) {
         ("call", [op, a, b]) => {
-          let lhs_value = codegen_expression(a, ctx, module_provider, sym)?;
-          let rhs_value = codegen_expression(b, ctx, module_provider, sym)?;
+          let lhs_value = codegen_float(a, ctx, module, sym)?;
+          let rhs_value = codegen_float(b, ctx, module, sym)?;
           let op_str = sym.str(op.symbol_unwrap()?);
           match op_str {
-            "+" => Ok(ctx.builder.build_float_add(lhs_value, rhs_value, "add_result")),
-            "-" => Ok(ctx.builder.build_float_sub(lhs_value, rhs_value, "sub_result")),
-            "*" => Ok(ctx.builder.build_float_mul(lhs_value, rhs_value, "mul_result")),
-            "/" => Ok(ctx.builder.build_float_div(lhs_value, rhs_value, "div_result")),
+            "+" => Ok(ctx.builder.build_float_add(lhs_value, rhs_value, "add_result").into()),
+            "-" => Ok(ctx.builder.build_float_sub(lhs_value, rhs_value, "sub_result").into()),
+            "*" => Ok(ctx.builder.build_float_mul(lhs_value, rhs_value, "mul_result").into()),
+            "/" => Ok(ctx.builder.build_float_div(lhs_value, rhs_value, "div_result").into()),
             _ => panic!(),
           }
+        }
+        ("fun", exprs) => {
+          let name = exprs[0].symbol_unwrap()?;
+          let args_exprs = exprs[1].children.as_slice();
+          let function_body = &exprs[2];
+
+          let mut arg_names = vec!();
+          let mut arg_types = vec!();
+          for (arg, type_expr) in args_exprs.iter().tuples() {
+            arg_names.push(arg.symbol_unwrap()?);
+            let arg_type = ctx.context.f64_type();
+            arg_types.push(arg_type);
+          }
+          panic!()
         }
         _ => panic!(),
       }
     }
     ExprTag::LiteralFloat(f) => {
-      Ok(ctx.context.f64_type().const_float(*f as f64))
+      Ok(ctx.context.f64_type().const_float(*f as f64).into())
     }
     _ => panic!(),
+  }
+}
+
+fn codegen_function(
+  expr : &Expr,
+  body : &Expr,
+  name : &str,
+  args : Vec<RefStr>,
+  ctx: &mut Ctx,
+  module: &mut Module,
+  sym : &mut SymbolTable)
+    -> Result<FunctionValue, Error>
+{
+  // check if declaration with this name was already done
+  if module.get_function(name).is_some() {
+    return error(expr, format!("function '{}' already defined", name));
+  };
+
+  // we have no global variables, so we can clear all the
+  // previously defined named values as they come from other functions
+  ctx.named_values.clear();
+
+  let ret_type = ctx.context.f64_type();
+  let args_types = std::iter::repeat(ret_type)
+    .take(args.len())
+    .map(|f| f.into())
+    .collect::<Vec<BasicTypeEnum>>();
+  let args_types = args_types.as_slice();
+
+  let fn_type = ctx.context.f64_type().fn_type(args_types, false);
+  let function = module.add_function(name, fn_type, None);
+
+  // set arguments names
+  for (i, arg) in function.get_param_iter().enumerate() {
+    arg.into_float_value().set_name(args[i].as_ref());
+  }
+
+  let entry = ctx.context.append_basic_block(&function, "entry");
+
+  ctx.builder.position_at_end(&entry);
+
+  // set function parameters
+  for (param, arg) in function.get_param_iter().zip(&args) {
+    ctx.named_values.insert(arg.clone(), param);
+  }
+
+  // compile body
+  let body_val = codegen_expression(body, ctx, module, sym)?;
+
+  // clear local variables
+  ctx.named_values.clear();
+
+  ctx.builder.build_return(Some(&body_val));
+
+  // return the whole thing after verification and optimization
+  if function.verify(true) {
+    // TODO: make this line work later
+    // ctx.pass_manager.run_on_function(&function);
+    Ok(function)
+  }
+  else {
+    unsafe {
+        function.delete();
+    }
+    error(expr, "invalid generated function.")
   }
 }
 
@@ -139,7 +198,7 @@ pub fn run_repl() {
   let mut rl = Editor::<()>::new();
 
   let mut ctx = Ctx::new();
-  let mut mod_provider = SimpleModuleProvider::new("top_level");
+  let mut mod_provider = Module::create("top_level");
 
   'main: loop {
     let mut input_line = rl.readline("repl> ").unwrap();
